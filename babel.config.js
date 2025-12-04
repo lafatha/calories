5f3afb1bef5675e63803dc45a0b8c20a2dd960e2
lafatha <athallahfatha@gmail.com>
2025-12-04 23:44:50 +0700

    Fix: Remove Reanimated to resolve Worklets mismatch on Expo Go

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,5 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    // Removed reanimated plugin temporarily to isolate the crash
-    plugins: [], 
   };
 };
